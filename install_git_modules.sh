54cc27d8764c0dba9530cf60df825adc2f75bd5c
2080524785 <2080524785@qq.com>
2024-10-07 20:10:38 +0800

    modify

--- install_git_modules.sh
+++ install_git_modules.sh
@@ -20,49 +20,53 @@ set_git_modules
 
 
 # # download d2net models 
-# print_blue '================================================'
-# print_blue "Updating d2net and downloading d2net models ..."
-# cd thirdparty/d2net
-# make_dir models 
-# cd models
-# if [ ! -f d2_ots.pth ]; then 
-#     echo downloading d2net model 
-#     # The original dsmn.ml links are now broken, so we download from my drive instead.
-#     #wget https://dsmn.ml/files/d2-net/d2_ots.pth -O models/d2_ots.pth
-#     #wget https://dsmn.ml/files/d2-net/d2_tf.pth -O models/d2_tf.pth
-#     #wget https://dsmn.ml/files/d2-net/d2_tf_no_phototourism.pth -O models/d2_tf_no_phototourism.pth    
-#     #https://drive.google.com/file/d/12Uk95TjBT7VZSEitvm3B3XNK37Q_uU8T/view?usp=sharing
-#     gdrive_download "12Uk95TjBT7VZSEitvm3B3XNK37Q_uU8T" "d2net.tar.xz"
-#     tar -xvf d2net.tar.xz
-#     rm d2net.tar.xz   
-# fi 
-# cd $STARTING_DIR
-# cd thirdparty
-# cd d2net
-# git apply ../d2net.patch 
-# cd $STARTING_DIR
+print_blue '================================================'
+print_blue "Updating d2net and downloading d2net models ..."
+cd thirdparty/d2net
+make_dir models 
+cd models
+if [ ! -f d2_ots.pth ]; then 
+    echo downloading d2net model 
+    # The original dsmn.ml links are now broken, so we download from my drive instead.
+    #wget https://dsmn.ml/files/d2-net/d2_ots.pth -O models/d2_ots.pth
+    #wget https://dsmn.ml/files/d2-net/d2_tf.pth -O models/d2_tf.pth
+    #wget https://dsmn.ml/files/d2-net/d2_tf_no_phototourism.pth -O models/d2_tf_no_phototourism.pth    
+    #https://drive.google.com/file/d/12Uk95TjBT7VZSEitvm3B3XNK37Q_uU8T/view?usp=sharing
+    # gdrive_download "12Uk95TjBT7VZSEitvm3B3XNK37Q_uU8T" "d2net.tar.xz"
+    # mv ../../contextdesc/pretrained/d2net.tar.xz ./
+    wget https://drive.usercontent.google.com/download?id=12Uk95TjBT7VZSEitvm3B3XNK37Q_uU8T&export=download&authuser=0
+    tar -xvf d2net.tar.xz
+    rm d2net.tar.xz   
+fi 
+cd $STARTING_DIR
+cd thirdparty
+cd d2net
+git apply ../d2net.patch 
+cd $STARTING_DIR
 
 
 # # download contextdesc models 
-# print_blue '================================================'
-# print_blue "Checking and downloading contextdesc models ..."
-# cd thirdparty/contextdesc
-# touch __init__.py 
-# make_dir pretrained 
-# cd pretrained
-# if [ ! -d retrieval_model ]; then 
-#     # NOTE: the original altizure links are now broken, so we download from my drive instead.
-#     #wget https://research.altizure.com/data/contextdesc_models/contextdesc_pp.tar
-#     #https://drive.google.com/file/d/1TQIjijkyd3fNvEivPPpnxHKaSqFxu5TE/view?usp=sharing
-#     gdrive_download "1TQIjijkyd3fNvEivPPpnxHKaSqFxu5TE" "contextdesc++.tar.xz"
-#     tar -xvf contextdesc++.tar.xz
-#     rm contextdesc++.tar.xz   
-#     #wget https://research.altizure.com/data/contextdesc_models/retrieval_model.tar
-#     # https://drive.google.com/file/d/1_J_aDSdKcUUk0ZXhn9bTqV6zuyzUixLD/view?usp=sharing
-#     gdrive_download "1_J_aDSdKcUUk0ZXhn9bTqV6zuyzUixLD" "retrieval_model.tar.xz"
-#     tar -xvf retrieval_model.tar.xz    
-#     rm retrieval_model.tar.xz 
-# fi 
+print_blue '================================================'
+print_blue "Checking and downloading contextdesc models ..."
+cd thirdparty/contextdesc
+touch __init__.py 
+make_dir pretrained 
+cd pretrained
+if [ ! -d retrieval_model ]; then 
+    # NOTE: the original altizure links are now broken, so we download from my drive instead.
+    #wget https://research.altizure.com/data/contextdesc_models/contextdesc_pp.tar
+    #https://drive.google.com/file/d/1TQIjijkyd3fNvEivPPpnxHKaSqFxu5TE/view?usp=sharing
+    # gdrive_download "1TQIjijkyd3fNvEivPPpnxHKaSqFxu5TE" "contextdesc++.tar.xz"
+    wget https://drive.usercontent.google.com/download?id=1_J_aDSdKcUUk0ZXhn9bTqV6zuyzUixLD&export=download&authuser=0&confirm=t&uuid=cd0bf559-5304-4200-98e8-ec4fd3548468&at=AN_67v1Pk6ImaDdtnFiCnDdM3SYi%3A1728291988708
+    tar -xvf contextdesc++.tar.xz
+    rm contextdesc++.tar.xz   
+    #wget https://research.altizure.com/data/contextdesc_models/retrieval_model.tar
+    # https://drive.google.com/file/d/1_J_aDSdKcUUk0ZXhn9bTqV6zuyzUixLD/view?usp=sharing
+    # gdrive_download "1_J_aDSdKcUUk0ZXhn9bTqV6zuyzUixLD" "retrieval_model.tar.xz"
+    wget https://drive.usercontent.google.com/download?id=1TQIjijkyd3fNvEivPPpnxHKaSqFxu5TE&export=download&authuser=0
+    tar -xvf retrieval_model.tar.xz    
+    rm retrieval_model.tar.xz 
+fi 
 cd $STARTING_DIR
 
 
